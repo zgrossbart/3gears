dcae1fc32dc51194278056b5b3d7deb2968f1dcd
Zack Grossbart <zack@grossbart.com>
2011-11-18 08:26:05 -0500

    Cleaned up the code a little bit and added a few more comments.

--- processing_gears.java
+++ processing_gears.java
@@ -1,19 +1,12 @@
-float x = 50;
-float y = 100;
+boolean stopped = false;
 float angle1 = 65.0;
 float angle2 = 0.0;
 float angle3 = 65.0;
-float segLength = 150;
-boolean stopped = false;
 
 void setup() {
   size(800, 900);
   smooth(); 
   frameRate(30);
-  //noStroke();
-  //strokeWeight(20.0);
-  //stroke(0, 100);
-  //
   draw();
 }
 
@@ -23,33 +16,35 @@ void draw() {
     }
   
     background(236);
-    pushMatrix();
-  
+
     int speed = 0.02;
   
+    /*
+     The big red gear
+     */
     angle1 += speed;
-    
     if (angle1 > 360) {
         angle1 = 0;
     }
     drawGear(width/3, height/4, 315, angle1, #FF0000);
     
+    /*
+     The big green gear
+     */
     angle3 += speed;
-    
     if (angle3 > 360) {
         angle3 = 0;
     }
     drawGear((width/3) + 20, (height / 4) + (315 / 1.53) + 220, 315, angle3, #00FF00);
     
+    /*
+     The small blue gear
+     */
     angle2 -= speed * 1.62;
-    
     if (angle2 > 360) {
         angle2 = 0;
     }
-  
     drawGear((width / 3) + (315 / 1.53), (height / 4) + (315 / 1.53), 195, angle2, #0000FF);
-    
-    popMatrix();
   
 }
 
@@ -57,21 +52,20 @@ void drawGear(int x, int y, int d, int angle, color c) {
     pushMatrix();
     translate(x, y);
     
-    
     rotate(angle);
-    pushMatrix();
-    
-    //translate(width/2, height/2);
-    
     noStroke();
     
+    /*
+     The gear circle
+     */
     fill(c);
     ellipse(0, 0, d, d);
     
+    /*
+     The inner gap
+     */
     fill();
     ellipse(0, 0, d / 4, d / 4);
-    
-    popMatrix();
     
     drawTeeth((d / 2) - 5, d / 15, c);
     
@@ -109,22 +103,10 @@ void drawTeeth(int d, int plots, color c) {
         //fill(255, 0, 0);
         //ellipse(0, 0, 10, 10);
         popMatrix();
-        
     }
-    
-    
-    
-    
-    
     popMatrix();
 }
 
 void mousePressed() {
     stopped = !stopped;
-}
-
-void segment(float x, float y, float a) {
-  translate(x, y);
-  rotate(a);
-  line(0, 0, segLength, 0);
 }
