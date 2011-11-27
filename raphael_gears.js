8769e07eedc6cd52a73a789de988ddd4de51a7a5
Zack Grossbart <zack@grossbart.com>
2011-11-27 10:37:06 -0500

    Changed the gear color in the Raphael example.

--- raphael_gears.js
+++ raphael_gears.js
@@ -1,3 +1,21 @@
+/******************************************************************************* 
+ * 
+ * Copyright 2011 Zack Grossbart 
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ******************************************************************************/
+ 
 var speed = 0.75;
 var scale = 15;
 var toothSize = 28;
@@ -74,6 +92,6 @@ window.onload = function () {
     g.create({
         'x': 250,
         'y': 250
-    }, 15, '#52b755', !clockwise);
+    }, 15, '#ee2a33', !clockwise);
     
 };
